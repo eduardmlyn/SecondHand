f02f60f2ecbf971c456c2bb5087e640f12aef8f7
Eduard Stefan Mlynarik <xmlynar2@fi.muni.cz>
2022-06-06 23:07:10 +0200

    feat: added gets

--- src/backend/db_comm/user/user.ts
+++ src/backend/db_comm/user/user.ts
@@ -6,14 +6,67 @@ export const addUser = async (
   email: string
 ) => {
   try {
-    await prisma.user.create({
+    const test = await prisma.user.create({
       data: {
         username,
         password,
         email
       }
     })
+    console.log(test)
     return "success"
+  } catch (e) {
+    return "error"
+  }
+}
+
+
+export const getUserByReview = async (reviewId: string) => {
+  try {
+    const user = await prisma.review.findUnique({
+      where: {
+        id: reviewId
+      },
+      select: {
+        user: true
+      }
+    })
+    if (user === null) {
+      return "null?!!"
+    }
+    console.log(user.user)
+    return user.user
+  } catch (e) {
+    return "err"
+  }
+}
+
+export const getUserById = async (id :string) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id
+      }
+    })
+    console.log(user)
+    return user
+  } catch (e) {
+    return "error"
+  }
+}
+
+export const getUserByAdvertisement = async (id: string) => {
+  try {
+    const user = await prisma.advertisement.findUnique({
+      where: {
+        id
+      },
+      select: {
+        user: true
+      }
+    })
+    console.log(user)
+    return user
   } catch (e) {
     return "error"
   }
@@ -26,7 +79,7 @@ export const updateUser = async (
   email: string
 ) => {
   try {
-    await prisma.user.update({
+    const test = await prisma.user.update({
       where: {
         id: id
       },
@@ -36,6 +89,7 @@ export const updateUser = async (
         email
       }
     })
+    console.log(test)
     return "success"
   } catch (e) {
     return "error"
@@ -44,7 +98,7 @@ export const updateUser = async (
 
 export const deleteUser = async (id: string) => {
   try {
-    await prisma.user.update({
+    const test = await prisma.user.update({
       where: {
         id: id
       },
@@ -52,6 +106,7 @@ export const deleteUser = async (id: string) => {
         deleted: true
       }
     })
+    console.log(test)
     return "success"
   } catch (e) {
     return "error"
